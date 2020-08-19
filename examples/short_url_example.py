b390f8e095557caaf07d56d9dc234216482eef84
Ian Webster <ianw_github@ianww.com>
2020-08-19 16:54:25 -0400

    Update short_url_example.py

--- examples/short_url_example.py
+++ examples/short_url_example.py
@@ -14,4 +14,8 @@ qc.config = {
 }
 
 print(qc.get_short_url())
+#
+# Example output (note that this shortened URL is now expired and will not display a chart):
+#
 # https://quickchart.io/chart/render/f-b4bf9221-0499-4bc6-b1ae-6f7c78be9d93
+#
